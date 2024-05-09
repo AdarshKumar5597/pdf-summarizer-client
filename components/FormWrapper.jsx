b19c02793e2f7435a7d230cc2ba61fddf3beb34b
AdarshKumar5597 <adarshkumar5597@gmail.com>
2024-05-09 14:26:01 +0530

    Added Server

--- components/FormWrapper.jsx
+++ components/FormWrapper.jsx
@@ -4,10 +4,18 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 function AuthForm({ type }) {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleShowPassword = () => {
     setShowPassword((prev) => !prev);
@@ -15,7 +23,38 @@ function AuthForm({ type }) {
 
   const handleConfirmShowPassword = () => {
     setShowConfirmPassword((prev) => !prev);
-  }
+  };
+
+  const onSubmit = async (data) => {
+    if (type === "register") {
+      if (data.password !== data.confirmPassword) {
+        setConfirmPasswordError("Passwords do not match.");
+        return;
+      } else {
+        setConfirmPasswordError("");
+      }
+    }
+    console.log(data);
+    try {
+      let response = await fetch(`http://localhost:4000/api/v1/${type}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      let result = await response.json();
+
+      if (result.success) {
+        console.log(result);
+      } else {
+        console.log(result);
+      }
+    } catch (error) {
+      console.log("error while submitting form", error);
+      console.error(error);
+    }
+  };
 
   return (
     <div
@@ -26,26 +65,66 @@ function AuthForm({ type }) {
       <h1 className="p-5 w-full flex items-center justify-center font-bold text-5xl text-[#8fe9f5]">
         {type === "login" ? "Login" : "Register"}
       </h1>
-      <form className="flex flex-col gap-y-4">
+      <form className="flex flex-col gap-y-4" onSubmit={handleSubmit(onSubmit)}>
         {type === "register" && (
           <>
-            <label className="flex flex-col  font-bold text-sm">Username
-            <input className="p-3 rounded-md bg-[#646992] text-white font-semibold" />
+            <label className="flex flex-col  font-bold text-sm">
+              Username
+              <input
+                name="username"
+                type="text"
+                className="p-3 rounded-md bg-[#646992] text-white font-semibold"
+                {...register("username", {
+                  required: true,
+                  maxLength: 20,
+                  minLength: 3,
+                })}
+              />
             </label>
           </>
         )}
-        <label className="flex flex-col  font-bold text-sm">Email
-        <input
-          type="text"
-          className="p-3 rounded-md bg-[#646992] text-white font-semibold"
-        />
+        <label className="flex flex-col  font-bold text-sm">
+          Email
+          <input
+            type="text"
+            name="email"
+            className="p-3 rounded-md bg-[#646992] text-white font-semibold"
+            {...register("email", {
+              required: true,
+              pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+            })}
+          />
+          {errors.email && errors.email.type === "required" && (
+            <p className="errorMsg text-red-600 text-xs font-semibold">
+              Email is required.
+            </p>
+          )}
+          {errors.email && errors.email.type === "pattern" && (
+            <p className="errorMsg text-red-600 text-xs font-semibold">
+              Email is not valid.
+            </p>
+          )}
         </label>
         <label className="flex flex-col relative font-bold text-sm">
           Password
           <input
             type={showPassword ? "text" : "password"}
+            name="password"
             className="p-3 rounded-md bg-[#646992] text-white font-semibold"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              maxLength: 20,
+            })}
           ></input>
+          {errors.password && errors.password.type === "required" && (
+            <p className="errorMsg text-red-600 text-xs font-semibold">Password is required.</p>
+          )}
+          {errors.password && errors.password.type === "minLength" && (
+            <p className="errorMsg text-red-600 text-xs font-semibold">
+              Password should be at-least 6 characters.
+            </p>
+          )}
           <div
             onClick={handleShowPassword}
             className="absolute top-1/2 transform -trasnlate-y-1/2 text-xl right-4 text-[#8fe9f5] cursor-pointer"
@@ -59,8 +138,23 @@ function AuthForm({ type }) {
               Confirm Password
               <input
                 type={showConfirmPassword ? "text" : "password"}
+                name="confirmPassword"
+                {...register("confirmPassword", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                })}
                 className="p-3 rounded-md bg-[#646992] text-white font-semibold"
               />
+              {errors.confirmPassword && errors.confirmPassword.type === "required" && (
+                <p className="errorMsg text-red-600 text-xs font-semibold">Confirm Password is required.</p>
+              )}
+              {errors.confirmPassword && errors.confirmPassword.type === "minLength" && (
+                <p className="errorMsg text-red-600 text-xs font-semibold">
+                  Confirm Password should be at-least 6 characters.
+                </p>
+              )}
+              {confirmPasswordError && <p className="errorMsg text-red-600 text-xs font-semibold">{confirmPasswordError}</p>}
               <div
                 onClick={handleConfirmShowPassword}
                 className="absolute top-1/2 transform -trasnlate-y-1/2 text-xl right-4 text-[#8fe9f5] cursor-pointer"
@@ -75,7 +169,14 @@ function AuthForm({ type }) {
         </button>
       </form>
       <div className="w-full h-auto p-3">
-        <p className="text-white font-bold text-sm">Already Have An Account? <span className=" cursor-pointer text-sm font-bold text-[#8fe9f5]"><Link href={type === "login" ? "/auth/register" : "/auth/login"}>{type === "login" ? "Register" :  "Login"}</Link></span></p>
+        <p className="text-white font-bold text-sm">
+          Already Have An Account?{" "}
+          <span className=" cursor-pointer text-sm font-bold text-[#8fe9f5]">
+            <Link href={type === "login" ? "/auth/register" : "/auth/login"}>
+              {type === "login" ? "Register" : "Login"}
+            </Link>
+          </span>
+        </p>
       </div>
     </div>
   );
