21b177a93be2035095fbb9dfe1156d65c8d1f03e
Siddhartha587729 <86753156+biswabismay@users.noreply.github.com>
2024-05-15 10:02:32 +0530

    [feature] feature component added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -14,12 +14,12 @@ export default function Navbar() {
 
     const pathname = usePathname();
     return (
-        <header className="m-8 ">
-            <div className="flex items-center justify-between mx-auto my-4 max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <header className="m-4 ">
+            <div className="flex items-center justify-between max-w-full px-4 sm:px-6 ">
                 <Link href="/">
                     <h1 className="text-2xl md:text-3xl font-bold"> Pdf <span className="text-blue-500">Summariser</span></h1>
                 </Link>
-                <nav className="gap-8 flex 2xl:ml-16 ">
+                <nav className="gap-6 flex justify-center items-center">
                     {links.map((link, index) => (
                         <div key={index}>
                             {pathname === link.href ? (
@@ -33,8 +33,12 @@ export default function Navbar() {
                             )}
                         </div>
                     ))}
+                    <div>
+                        <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Login</button>
+                        <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Sign Up</button>
+                    </div>
                 </nav>
-                
+
             </div>
         </header>
     )
